bb4ecff20f66b0b8cebc5a49119301da6859463a
Kousha Talebian <kousha.talebian@gmail.com>
2020-06-08 10:32:06 -0700

    use the env module

--- packages/flex-plugins-api-utils/src/env.ts
+++ packages/flex-plugins-api-utils/src/env.ts
@@ -1,16 +1,2 @@
-export type Realm = 'dev' | 'stage';
-
-/**
- * Returns true if running in debug verbose mode
- */
-export const isDebug = () => process.env.DEBUG === 'true';
-
-/**
- * Returns the realm
- */
-export const getRealm = () => process.env.REALM as Realm;
-
-export default {
-  isDebug,
-  getRealm,
-};
+export { Realm } from 'flex-plugins-utils-env/dist/lib/env';
+export { default } from 'flex-plugins-utils-env';
